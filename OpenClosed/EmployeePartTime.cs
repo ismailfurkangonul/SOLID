03b8d78c702ebd2e32b5fa3736ba8a66b3a7d5fc
ismailfurkangonul <ismailfurkangonul2@gmail.com>
2022-07-31 18:55:26 +0300

    Add project files.

---
+++ OpenClosed/EmployeePartTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    internal class EmployeePartTime : IEmployee
+    {
+
+        public void CalculateSalary()
+        {
+
+            Console.WriteLine("maaş hesaplandi");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OpenClosed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEmployee employee = new EmployeePartTime();
+            employee.CalculateSalary();
+         
+        }
+    }
+}
